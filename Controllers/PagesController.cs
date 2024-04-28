3f48af6946246e1f2ce0214fa962f0c8a9665f75
DavidBrenes <dbrenesmartinez@gmail.com>
2024-04-28 01:49:13 -0600

    Innitial Commit - Login and Main Menu

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+using OrientaTEC_MVC.Models;
+using System.Diagnostics;
+
+namespace OrientaTEC_MVC.Controllers
+{
+    public class PagesController : Controller
+    {
+        private readonly ILogger<PagesController> _logger;
+
+        public PagesController(ILogger<PagesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult GestorEquiposGuia()
+        {
+            return View();
+        }
+
+        public IActionResult GestorEstudiantes()
+        {
+            return View();
+        }
+
+
+        public IActionResult GestorProfesores()
+        {
+            return View();
+        }
+
+        public IActionResult MenuPrincipal()
+        {
+            return View();
+        }
+
+        public IActionResult PlaneacionActividades()
+        {
+            return View();
+        }
+    }
+}
