d5aeac70e7b3e6cd8dd393921dbfd1f09c9934ac
JPRN01 <65576823+JPRN01@users.noreply.github.com>
2024-04-30 14:57:19 -0600

    Reapply "cambio singleton"
    This reverts commit e2e648f0ae7e805f67cdea0bb1fddf46bf3a6690.

--- Controllers/EstudianteDAO_Singleton.cs
+++ Controllers/EstudianteDAO_Singleton.cs
@@ -4,43 +4,66 @@ using System.Diagnostics;
 
 namespace OrientaTEC_MVC.Controllers
 {
-	public class EstudianteDAO_Singleton : Controller
-	{
-		public Class1()
-		{
-			//
-			// TODO: Add constructor logic here
-			//
-		}
-	}
-    public bool AgregarEstudiante(Estudiante estudiante)
+    public class EstudianteDAO_Singleton : Controller
     {
-        using (var connection = new SqlConnection(connectionString))
+        private static EstudianteDAO_Singleton _instance;
+        private static readonly object _lock = new object();
+        private readonly string connectionString;
+
+        private EstudianteDAO_Singleton()
         {
-            const string query = @"
+            // Asignar la cadena de conexión desde appsettings.json
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            connectionString = config.GetConnectionString("DefaultConnection");
+        }
+
+        public static EstudianteDAO_Singleton Instance
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new EstudianteDAO_Singleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public bool AgregarEstudiante(Estudiante estudiante)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                const string query = @"
                 INSERT INTO Estudiante (CARNE, nombre1, nombre2, apellido1, apellido2, correo, tel_celular, centro_academico)
                 VALUES (@Carnet, @Nombre1, @Nombre2, @Apellido1, @Apellido2, @Correo, @TelCelular, @CentroAcademico)";
 
-            using (var command = new SqlCommand(query, connection))
-            {
-                command.Parameters.AddWithValue("@Carnet", estudiante.Carnet);
-                command.Parameters.AddWithValue("@Nombre1", estudiante.Nombre1);
-                command.Parameters.AddWithValue("@Nombre2", estudiante.Nombre2 ?? (object)DBNull.Value);
-                command.Parameters.AddWithValue("@Apellido1", estudiante.Apellido1);
-                command.Parameters.AddWithValue("@Apellido2", estudiante.Apellido2);
-                command.Parameters.AddWithValue("@Correo", estudiante.Correo);
-                command.Parameters.AddWithValue("@TelCelular", estudiante.TelCelular);
-                command.Parameters.AddWithValue("@CentroAcademico", estudiante.CentroAcademico);
+                using (var command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Carnet", estudiante.Carnet);
+                    command.Parameters.AddWithValue("@Nombre1", estudiante.Nombre1);
+                    command.Parameters.AddWithValue("@Nombre2", estudiante.Nombre2 ?? (object)DBNull.Value);
+                    command.Parameters.AddWithValue("@Apellido1", estudiante.Apellido1);
+                    command.Parameters.AddWithValue("@Apellido2", estudiante.Apellido2);
+                    command.Parameters.AddWithValue("@Correo", estudiante.Correo);
+                    command.Parameters.AddWithValue("@TelCelular", estudiante.TelCelular);
+                    command.Parameters.AddWithValue("@CentroAcademico", estudiante.CentroAcademico);
 
-                try
-                {
-                    connection.Open();
-                    var result = command.ExecuteNonQuery();
-                    return result > 0;
-                }
-                catch (SqlException)
-                {
-                    return false;
+                    try
+                    {
+                        connection.Open();
+                        var result = command.ExecuteNonQuery();
+                        return result > 0;
+                    }
+                    catch (SqlException)
+                    {
+                        return false;
+                    }
                 }
             }
         }
