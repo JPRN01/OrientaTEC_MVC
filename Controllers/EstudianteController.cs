8f7180c3656e2897267c214abff070be86034024
JPRN01 <65576823+JPRN01@users.noreply.github.com>
2024-04-30 14:38:13 -0600

    Cambios controlador estudiantes

---
+++ Controllers/EstudianteController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrientaTEC_MVC.Models;
+using System.Diagnostics;
+
+namespace OrientaTEC_MVC.Controllers
+{
+	public class EstudianteController : Controller
+	{
+		public EstudianteController()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+        public void CargarInformacionEstudiantes(string archivoExcel)
+        {
+            using (var stream = File.OpenRead(archivoExcel))
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                using (var package = new ExcelPackage(stream))
+                {
+                    var worksheet = package.Workbook.Worksheets[0];
+                    var totalRows = worksheet.Dimension.Rows;
+                    for (int row = 2; row <= totalRows; row++)
+                    {
+                        var estudiante = new Estudiante
+                        {
+                            Carnet = int.Parse(worksheet.Cells[row, 1].Value.ToString().Trim()),
+                            Apellido1 = worksheet.Cells[row, 2].Value.ToString().Split(' ')[0].Trim(),
+                            Apellido2 = worksheet.Cells[row, 2].Value.ToString().Split(' ')[1].Trim(),
+                            Nombre1 = worksheet.Cells[row, 2].Value.ToString().Split(' ')[2].Trim(),
+                            Nombre2 = worksheet.Cells[row, 2].Value.ToString().Split(' ').Length > 3 ? worksheet.Cells[row, 2].Value.ToString().Split(' ')[3].Trim() : null,
+                            Correo = worksheet.Cells[row, 3].Value.ToString().Trim(),
+                            TelCelular = int.Parse(worksheet.Cells[row, 4].Value.ToString().Trim()),
+                            CentroAcademico = "Centro" //Hay que cambiarlo para recuperar el centro segun el asistente que lo sube 
+                        };
+
+                        bool resultado = EstudianteDAO_Singleton.Instance.AgregarEstudiante(estudiante);
+                        if (!resultado)
+                        {
+                            Console.WriteLine($"Error al insertar el estudiante con carné: {estudiante.Carnet}");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Controllers/EstudianteDAO_Singleton.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrientaTEC_MVC.Models;
+using System.Diagnostics;
+
+namespace OrientaTEC_MVC.Controllers
+{
+	public class EstudianteDAO_Singleton : Controller
+	{
+		public Class1()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+    public bool AgregarEstudiante(Estudiante estudiante)
+    {
+        using (var connection = new SqlConnection(connectionString))
+        {
+            const string query = @"
+                INSERT INTO Estudiante (CARNE, nombre1, nombre2, apellido1, apellido2, correo, tel_celular, centro_academico)
+                VALUES (@Carnet, @Nombre1, @Nombre2, @Apellido1, @Apellido2, @Correo, @TelCelular, @CentroAcademico)";
+
+            using (var command = new SqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@Carnet", estudiante.Carnet);
+                command.Parameters.AddWithValue("@Nombre1", estudiante.Nombre1);
+                command.Parameters.AddWithValue("@Nombre2", estudiante.Nombre2 ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@Apellido1", estudiante.Apellido1);
+                command.Parameters.AddWithValue("@Apellido2", estudiante.Apellido2);
+                command.Parameters.AddWithValue("@Correo", estudiante.Correo);
+                command.Parameters.AddWithValue("@TelCelular", estudiante.TelCelular);
+                command.Parameters.AddWithValue("@CentroAcademico", estudiante.CentroAcademico);
+
+                try
+                {
+                    connection.Open();
+                    var result = command.ExecuteNonQuery();
+                    return result > 0;
+                }
+                catch (SqlException)
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
