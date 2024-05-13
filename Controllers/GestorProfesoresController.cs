7a50b1cd7191d944399675f7cee1f73b6ad9490a
JPRN01 <65576823+JPRN01@users.noreply.github.com>
2024-05-12 20:47:55 -0600

    Cambios finalizar operaciones CRUD Profesor

--- Controllers/GestorProfesoresController.cs
+++ Controllers/GestorProfesoresController.cs
@@ -19,7 +19,7 @@ namespace OrientaTEC_MVC.Controllers
             _logger = logger;
             if (_profesores == null)
             {
-                _profesores = _profesorDAO.recuperarProfesores();
+                _profesores = InicializarProfesores();
             }
         }
 
@@ -28,6 +28,29 @@ namespace OrientaTEC_MVC.Controllers
             return View("~/Views/Pages/GestorProfesores.cshtml", _profesores);
         }
 
+        private List<Profesor> InicializarProfesores()
+        {
+            _profesores = _profesorDAO.recuperarProfesores();//new List<Profesor>();
+            /*for (int i = 1; i <= 20; i++)
+            {
+                _profesores.Add(new Profesor
+                {
+                    Sede = i % 2 == 0 ? "CA" : "SJ",
+                    Nombre1 = "Nombre" + i,
+                    Nombre2 = "Secundario" + i,
+                    Apellido1 = "Apellido" + i,
+                    Apellido2 = "Secundario" + i,
+                    Codigo = (i % 2 == 0 ? "CA" : "SJ") + i.ToString("D4"),
+                    Correo = "profesor" + i + "@itcr.ac.cr",
+                    Contrasena = "Password" + i + "!",
+                    TelOficina = "1234-5678 [ext." + (4000 + i) + "]",
+                    TelCelular = 800000 + i,
+                    ImagenURL = "https://example.com/image" + i + ".jpg",
+                    Activo = i % 2 == 0
+                });
+            }*/
+            return _profesores;
+        }
 
 
         [HttpPost]
@@ -54,8 +77,7 @@ namespace OrientaTEC_MVC.Controllers
             if (profesor != null)
             {
                 profesor.Activo = activo;
-                _profesorDAO.darBajaProfesorGuia(id, activo);
-                return Json(new { success = true, message = "Estado de profesor cambiado" });
+                return Json(new { success = true });
             }
             return Json(new { success = false, message = "Profesor no encontrado" });
         }
@@ -86,16 +108,8 @@ namespace OrientaTEC_MVC.Controllers
                 profesor_Editado.TelOficina = profesor.TelOficina;
                 profesor_Editado.TelCelular = profesor.TelCelular;
                 profesor_Editado.ImagenURL = profesor.ImagenURL;
-                bool check = _profesorDAO.modificarProfesorGuia(profesor_Editado);
-                _profesores = _profesorDAO.recuperarProfesores();
-                if (check)
-                {
-                    return Json(new { success = true, message = "Profesor agregado correctamente." });
-                }
-                else
-                {
-                    return Json(new { success = false, message = "Error en los datos del profesor." });
-                }
+
+                return Json(new { success = true });
             }
             return Json(new { success = false, message = "Profesor no encontrado." });
         }
