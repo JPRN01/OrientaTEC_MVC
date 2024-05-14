06334fbbc4b4fe24203b7704943c1726f6ac2ae2
Joseph David Santamaría Castro <85372807+JosephSanta@users.noreply.github.com>
2024-05-13 23:00:13 -0600

    xxxxxx

--- obj/Debug/net8.0/OrientaTEC_MVC.AssemblyInfo.cs
+++ obj/Debug/net8.0/OrientaTEC_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrientaTEC_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ce69b10aeb278ec5fe9cebb8214e3664353e20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c4560d3b8618ad3d67581efd4e432b0ec535cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrientaTEC_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrientaTEC_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
