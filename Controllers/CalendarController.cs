8fae50b0c921e1ca2b350529b3f4c1a17046f804
Joseph David Santamaría Castro <85372807+JosephSanta@users.noreply.github.com>
2024-06-19 03:31:28 -0600

    CALENDARIO

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using OrientaTEC_MVC.ViewModels;
+using OrientaTEC_MVC.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Diagnostics;
+
+
+public class CalendarController : Controller
+{
+
+    private readonly NotificationDAO _notificationDao;
+
+
+    public CalendarController()
+    {
+        _notificationDao = new NotificationDAO(); 
+    }
+
+    [Route("Calendar/EventCalendar")]
+    public IActionResult EventCalendar()
+    {
+        var notifications = _notificationDao.GetAllNotifications();
+
+        return View("~/Views/Pages/EventCalendar.cshtml", notifications);
+
+    }
+
+}
